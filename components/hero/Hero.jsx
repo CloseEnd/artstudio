f95c1127f39ba9022d78fb328d31910d98d33831
Taleem <taleemmankuer@gmail.com>
2023-06-28 05:14:04 +0300

    add hero

---
+++ components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import styles from "./Hero.module.scss";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Hello my name is okayi job and artist and content creator.</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+        eaque sunt corrupti necessitatibus facilis assumenda expedita dolor
+        saepe id voluptates nostrum praesentium, maxime optio iure!
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
