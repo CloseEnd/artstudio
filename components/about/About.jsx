0299006c88073d421b4ff4c9a939b5cd890c8d2e
Taleem <taleemmankuer@gmail.com>
2023-06-30 21:04:13 +0300

    add about

---
+++ components/about/About.jsx
@@ -0,0 +1,34 @@
+import styles from "./About.module.scss";
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import { PortableText } from "@portabletext/react";
+import { RichText } from "../Richtext/RichText";
+import Image from "next/image";
+import { urlForImage } from "@/sanity/lib/image";
+
+const About = async () => {
+  const query = groq`
+    *[_type == "author"]
+    `;
+  const aboutme = await client.fetch(query);
+  return (
+    <div className={styles.aboutme}>
+      <div className={styles.txt}>
+        <h1>ABOUT ME</h1>
+        <p>
+          <PortableText value={aboutme[0].bio} components={RichText} />
+        </p>
+        <button className={styles.button}>Learn more</button>
+      </div>
+      <div className={styles.img}>
+        <Image
+          src={urlForImage(aboutme[0].image).url()}
+          width={1000}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About;
