49808380b1f937fe822362e59bf5576d4464948c
Taleem <taleemmankuer@gmail.com>
2023-07-03 20:06:11 +0300

    update

--- app/(frontend)/page.js
+++ app/(frontend)/page.js
@@ -4,6 +4,7 @@ import styles from "./Page.module.scss";
 import Hero from "@/components/hero/Hero";
 import Projects from "@/components/projects/Projects";
 import About from "@/components/about/About";
+import Blogs from "@/components/blogs/Blogs";
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
         <Hero />
         <Projects />
         <About />
+        <Blogs />
       </main>
-      {/* footer */}
     </>
   );
 }
