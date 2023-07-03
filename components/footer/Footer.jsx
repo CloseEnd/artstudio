2ca19e13ddec0923b0d21a281d7490a4c0647eac
Taleem <taleemmankuer@gmail.com>
2023-07-03 20:05:46 +0300

    add footer

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.footercontainer}>
+      <h1>Okayi job 2023</h1>
+    </div>
+  );
+};
+
+export default Footer;
