92658b8f15e76c854266a08b0efa6ef4f8b80897
Taleem <taleemmankuer@gmail.com>
2023-07-07 21:46:14 +0300

    update footer design

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -3,7 +3,34 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footercontainer}>
-      <h1>Okayi job 2023</h1>
+      <div className={styles.footer}>
+        <h1>2023 Close End art gallery.</h1>
+        <div>
+          <ul>
+            <a href="">
+              <li>email@example.com</li>
+            </a>
+            <a href="">
+              <li>Privacy policy</li>
+            </a>
+            <a href="">
+              <li>f</li>
+            </a>
+            <a href="">
+              <li>In</li>
+            </a>
+            <a href="">
+              <li>Wa</li>
+            </a>
+            <a href="">
+              <li>Tel</li>
+            </a>
+            <a href="">
+              <li>tw</li>
+            </a>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
