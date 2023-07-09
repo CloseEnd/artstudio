f64bc34f53b84ec08c987632229a6fc430ee0add
Taleem <taleemmankuer@gmail.com>
2023-07-09 16:36:29 +0300

    add projects and single projects page

--- app/(frontend)/projects/[slug]/page.jsx
+++ app/(frontend)/projects/[slug]/page.jsx
@@ -1,12 +1,27 @@
+import styles from "./Project.module.scss";
 import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
 
-const Page = async ({ param: slug }) => {
+const Page = async ({ params: { slug } }) => {
+  const query = groq`
+  *[_type == "projects" && slug.current == $slug][0]
+  `;
+
+  const project = await client.fetch(query, { slug });
   return (
-    <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum aspernatur
-      illo earum quae inventore laboriosam maiores recusandae, eligendi officia
-      nulla quisquam? Harum aspernatur excepturi iste quaerat itaque nemo eum
-      sequi?
+    <div className={styles.container}>
+      <Image
+        src={urlForImage(project.image).url()}
+        alt={project.name}
+        width={1000}
+        height={500}
+      />
+      <div className={styles.txt}>
+        <h3>{project.name}</h3>
+        <p>{project.description}</p>
+      </div>
     </div>
   );
 };
