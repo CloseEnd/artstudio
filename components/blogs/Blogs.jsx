10a50f7e56c4385ce93e2852fa7b7560274e9211
Taleem <taleemmankuer@gmail.com>
2023-08-02 12:32:13 +0300

    update

--- components/blogs/Blogs.jsx
+++ components/blogs/Blogs.jsx
@@ -11,9 +11,6 @@ const Blogs = async () => {
   *[_type == "post"] | order(_createdAt desc)
   `;
   const posts = await client.fetch(query);
-  // console.log("====================================");
-  // console.log(posts);
-  // console.log("====================================");
   if (!posts.length) {
     return <></>;
   }
@@ -22,17 +19,6 @@ const Blogs = async () => {
       <h1>Blogs</h1>
       <h2>Latest post from my blog.</h2>
       <div className={styles.blogs}>
-        {/* <div className={styles.blog}>
-          <Image src={nextsvg} alt="image" />
-          <p>20 may 2022</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
-            temporibus modi fugit dicta, non accusantium minima corrupti
-            excepturi consequatur sapiente consectetur porro eaque explicabo ad
-            rem, veniam ea? Iure, sint.
-          </p>
-          <Link href={""}>Read more</Link>
-        </div> */}
         {posts
           .filter((post, i) => i < 3)
           .map((post, _i) => (
